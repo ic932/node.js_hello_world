53af6e20573310b623ffe5e2b7cc3c66a07983df
ic932 <okumura@i-collabo.com>
2017-03-03 14:58:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
-// netモジュールを読み込み、インスタンスを作成
-var net = require('net');
+'use strict'
 
-// サーバーのイベントハンドラを定義
-net.createServer(function (socket) {
+const HTTP = require('http');
+const PORT = process.env.PORT || 3001;
 
-    // ソケット経由でメッセージを送出
-    socket.write('This is RLS Tech News Echo Server\r\n');
-
-    // データを受信したら、そのままメッセージとして送出
-    socket.on('data', function(data) {
-        socket.write(data);
-    });
-}).listen(1337, '127.0.0.1'); // 127.0.0.1の1337番ポートで待機
+HTTP.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Node.js\n');
+}).listen(PORT);
