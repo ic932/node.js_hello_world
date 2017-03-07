ae8efe732599bf28e246ee7596b2122db7cdc961
ic932 <okumura@i-collabo.com>
2017-03-07 14:17:48 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-'use strict'
+/*
+ * hello, world
+ * IPなど設定：http://testcording.com/?p=1164
+ */
 
-const HTTP = require('http');
-const PORT = process.env.PORT || 3001;
-
-HTTP.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('<html>Hello Node.js</html>\n');
-}).listen(PORT);
+var http = require("http");
+http.createServer(function (req, res) {
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("hello, world!\n");
+}).listen(1337, "localhost", function () {
+	console.log("Server running at http://127.0.0.1:1337/");
+	console.log("サーバを終了する際は[ctrl + c]を押してください");
+});
