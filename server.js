83cf13a21cdf3b5305d18cee5ac1ee737661dff9
ic932 <okumura@i-collabo.com>
2017-03-07 14:22:33 +0900

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@
  */
 
 var http = require("http");
+var PORT = process.env.PORT || 3001;
 http.createServer(function (req, res) {
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end("hello, world!\n");
-}).listen(1337, "localhost", function () {
+}).listen(PORT, function () {
 	console.log("Server running at http://127.0.0.1:1337/");
 	console.log("サーバを終了する際は[ctrl + c]を押してください");
 });
