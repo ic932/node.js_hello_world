760ed819741b697805bc9dc443179dab8fa170cd
ic932 <okumura@i-collabo.com>
2017-03-07 14:49:39 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,39 @@
 /*
- * hello, world
- * IPなど設定：http://testcording.com/?p=1164
-
-var http = require("http");
-var PORT = process.env.PORT || 3001;
-http.createServer(function (req, res) {
-	res.writeHead(200, {"Content-Type": "text/plain"});
-	res.end("hello, world!\n");
-}).listen(PORT, function () {
-	console.log("Server running at http://127.0.0.1:1337/");
-	console.log("サーバを終了する際は[ctrl + c]を押してください");
-});
-*/
-/*
- * hello, world
- * IPなど設定：http://testcording.com/?p=1164
- */
-/*
- * パラメータ
- */
 var IP_ADDRESS = "localhost";
 var PORT    =   process.env.PORT || 3001;
 
-/*
- * モジュール読み込み
- */
 var http = require("http");
 
-/*
- * サーバの作成
- */
 http.createServer(function (req, res) {
 	// ヘッダーの送信 送信するデータのタイプはtext/plain
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	// 中身の文字列を送信
 	res.end("hello, world! : URL " + req.url);
 }).listen(PORT,function () {
-	/*
-	 * サーバ起動時に表示するログ(起動したことが分かりやすい)
-	 */
+
 	console.log("Server running at http://" + IP_ADDRESS + ":" + PORT + "/");
 	console.log("サーバを終了する際は[ctrl + c]を押してください");
 });
+*/
+var PORT    =   process.env.PORT || 3001;
+
+ var fs = require("fs");
+ var util = require('util');
+
+/*
+ * 非同期でファイルを読み込む関数
+ */
+ (function test() {
+ 	// 非同期でファイル読み込み(パスの指定は、nodeを起動しているパスからの相対パスになる)
+ 	fs.readFile("./testdata", "utf8", function (err, data) {
+ 		console.log("readFileを実行した！");
+ 		console.log(data); // 読み込んだファイルの内容を出力する
+ 	});
+ 	// 処理される順番が交錯するのに注目してください
+ 	console.log("readFile呼び出し終了");
+  	// 以下の処理が終了した後、アイドル状態になってから上の非同期処理が行われる(キュー内の処理を順に呼び出すため)
+ 	// 一定時間サーバを酷使するのでテスト環境で実行してください
+ 	for(var i = 0, len = 10000; i < len; ++i) {
+ 		util.print(".");
+ 	}
+})();
