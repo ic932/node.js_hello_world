6ef7572cd02ada00c2c45408b071c328975bd89c
ic932 <okumura@i-collabo.com>
2017-03-07 15:22:43 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,20 @@
 var PORT    =   process.env.PORT || 3001;
 
 var http = require("http");
+var fs=require("fs");
 var server=http.createServer();
 
 server.on('request',function(req,res){
-	res.writeHead(200,{"Content-Type": "text/plain"});  
-	res.write('<html>hello</html>');
-	res.end();
+	fs.readFile("index.html","utf-8",function(err,data){
+		if(err){
+			res.writeHead(404,{"Content-Type": "text/plain"});  
+			res.write('not found');
+			return res.end();
+		}
+		res.writeHead(200,{"Content-Type": "text/html"});  
+		res.write(data);
+		res.end();
+	});
+
 });
 server.listen(PORT) ;
